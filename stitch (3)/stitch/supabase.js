517acbaddbf28b86e4238d7cf925379bafe2bcd7
Seiyumi1215 <190924273+Seiyumi1215@users.noreply.github.com>
2026-03-19 01:22:38 +0800

    Delete stitch (3)/stitch/supabase.js

--- stitch (3)/stitch/supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://scjoddkykfnsxwjkzxjf.supabase.co"
-const SUPABASE_KEY = "sb_publishable_your_key_here"
-
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-)
